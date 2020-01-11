fd00415e0571adb8cf26401f327e814eb4237f95
koitsu884 <koitsu884@hotmail.com>
2020-01-11 15:54:37 +1300

    Update cron job

--- cron/getLatestSeasonalJobs.js
+++ cron/getLatestSeasonalJobs.js
@@ -19,56 +19,53 @@ function connectDB() {
     })
 }
 
-async function saveLatestDataToDB(data) {
-    parseStringPromise(data)
-        .then(async result => {
-            let latestJobs = [];
-            for (let i = 0; i < 10; i++) {
-                let jobDetail = result.source.job[i];
-                latestJobs.push({
-                    title: jobDetail.title[0],
-                    date: jobDetail.date[0],
-                    url: jobDetail.url[0],
-                    city: jobDetail.city[0],
-                    description: jobDetail.description[0],
-                    salary: jobDetail.salary[0],
-                    category: jobDetail.category[0],
-                });
-            }
-            console.log(latestJobs);
-            latestFeed = new LatestFeed({ jobList: latestJobs });
-            await latestFeed.save();
-        })
-        .catch(error => {
-            throw error;
-        });
-
-}
-
-function execute() {
-    console.log("Running crontask - getLatestSeasonalJobs");
-    connectDB().then(() => {
-        request('https://www.backpackerboard.co.nz/jobs-feed.xml', async (error, response, body) => {
+function requestJobFeed() {
+    return new Promise(function (resolve, reject) {
+        request('https://www.backpackerboard.co.nz/jobs-feed.xml', function (error, response, body) {
             if (error) {
                 console.log(error);
+                reject(error);
             }
             else {
-                try{
-                    await saveLatestDataToDB(body);
-                    mongoose.connection.close();
-                }
-                catch(error){
-                    console.log(error);
-                    mongoose.connection.close();
-                }
-                console.log('Cron job finished');
+                parseStringPromise(body)
+                    .then(async result => {
+                        let latestJobs = [];
+                        for (let i = 0; i < 10; i++) {
+                            let jobDetail = result.source.job[i];
+                            latestJobs.push({
+                                title: jobDetail.title[0],
+                                date: jobDetail.date[0],
+                                url: jobDetail.url[0],
+                                city: jobDetail.city[0],
+                                description: jobDetail.description[0],
+                                salary: jobDetail.salary[0],
+                                category: jobDetail.category[0],
+                            });
+                        }
+                        latestFeed = new LatestFeed({ jobList: latestJobs });
+                        await latestFeed.save();
+                        resolve();
+                    })
+                    .catch(error => {
+                        reject(error);
+                    });
             }
         });
     })
-    .catch(error => {
+}
+
+async function execute() {
+    console.log("Running crontask - getLatestSeasonalJobs");
+    try {
+        await connectDB();
+        await requestJobFeed();
+        mongoose.connection.close();
+        console.log('cron job finished');
+    }
+    catch (error) {
         console.log('connection failed');
         console.log(error);
-    })
+    }
 }
 
 execute();
