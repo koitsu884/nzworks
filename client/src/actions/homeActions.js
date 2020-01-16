852c1d5194cd69d4d2eef0a4063ac7f40d811f19
koitsu884 <koitsu884@hotmail.com>
2020-01-16 19:26:58 +1300

    Imcrease home ad limit

--- client/src/actions/homeActions.js
+++ client/src/actions/homeActions.js
@@ -34,7 +34,7 @@ export const getLatestFeedList = () => dispatch => {
 export const getLatestJobList = () => dispatch => {
     dispatch(setLoading());
     const params = {
-        limit: 5,
+        limit: 12,
     };
     
     client.get('job', {params:params}).then(response => {
